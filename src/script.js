ed1e9624177d4f155dd6825361c4572d399622d4
Jakub <j.gaba@email.cz>
2023-12-14 15:26:18 +0100

    Tweaks added

--- src/script.js
+++ src/script.js
@@ -5,7 +5,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import firefliesvertexshader from './shaders/fireflies/vertex.glsl'
 import firefliesfragmentshader from './shaders/fireflies/fragment.glsl'
-
+import portalvertexshader from './shaders/portal/vertex.glsl'
+import portalfragmentshader from './shaders/portal/fragment.glsl'
 
 /**
  * Spector JS
@@ -23,7 +24,7 @@ const gui = new GUI({
     width: 400
 })
 
-const debugObject={}
+const debugObject = {}
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -57,8 +58,36 @@ bakedTexture.colorSpace = THREE.SRGBColorSpace
 // Baked material
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
 
+debugObject.portalColorStart = '#ff0000'
+debugObject.portalColorEnd = '#0000ff'
+
 // Portal light material
-const portalLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
+const portalLightMaterial = new THREE.ShaderMaterial({
+    uniforms: { 
+        uTime: { value: 0 },
+        uColorStart: {value: new THREE.Color(debugObject.portalColorStart)},
+        uColorEnd:{value: new THREE.Color(debugObject.portalColorEnd)},
+        strength: { value: 0.0 },
+        strengthOuterGlow: {value: 0.0}
+    },
+    vertexShader: portalvertexshader,
+    fragmentShader: portalfragmentshader
+})
+
+
+gui
+.addColor(debugObject,'portalColorStart').onChange(()=>{portalLightMaterial.uniforms.uColorStart.value.set(debugObject.portalColorStart)})
+
+gui
+.addColor(debugObject,'portalColorEnd').onChange(()=>{portalLightMaterial.uniforms.uColorEnd.value.set(debugObject.portalColorEnd)})
+
+
+// gui.add(debugObject, 'strength', 0.0, 1.0).step(0.01).onChange(value => {
+//     portalLightMaterial.uniforms.strength.value = value;
+// });
+gui.add(portalLightMaterial.uniforms.strength, 'value').min(0).max(1).step(0.01).name("Strength")
+
+gui.add(portalLightMaterial.uniforms.strengthOuterGlow, 'value').min(0).max(1).step(0.01).name("outerStrength")
 
 // Pole light material
 const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffe5 })
@@ -87,28 +116,37 @@ gltfLoader.load(
  * Fireflies
  */
 const fireFliesGeometry = new THREE.BufferGeometry()
-const fireFliesCount = 30;
+const fireFliesCount = 300;
 const positionArray = new Float32Array(fireFliesCount * 3)
+const scaleArray = new Float32Array(fireFliesCount)
 
-for(let i = 0 ; i < fireFliesCount; i++){
-    positionArray[i*3+0]=(Math.random() -0.5) * 4
-    positionArray[i*3+1]=Math.random() * 1.5
-    positionArray[i*3+2]=(Math.random() -0.5) * 4
+for (let i = 0; i < fireFliesCount; i++) {
+    positionArray[i * 3 + 0] = (Math.random() - 0.5) * 4
+    positionArray[i * 3 + 1] = Math.random() * 1.5
+    positionArray[i * 3 + 2] = (Math.random() - 0.5) * 4
+
+    scaleArray[i] = Math.random()
 }
 
-fireFliesGeometry.setAttribute('position',new THREE.BufferAttribute(positionArray, 3))
-
+fireFliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+fireFliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 3))
 //Material
 
 const fireFliesMaterial = new THREE.ShaderMaterial({
     uniforms:
     {
-        uPixelRatio:{value: Math.min(window.devicePixelRatio, 2) }
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 70 },
+        uTime: { value: 0 }
     },
-    vertexShader: firefliesvertexshader ,
-    fragmentShader: firefliesfragmentshader
+    vertexShader: firefliesvertexshader,
+    fragmentShader: firefliesfragmentshader,
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    depthWrite: false
 })
 
+gui.add(fireFliesMaterial.uniforms.uSize, 'value').min(0).max(500).step(1).name("FireFliesSize")
 //Points
 const fireflies = new THREE.Points(fireFliesGeometry, fireFliesMaterial)
 scene.add(fireflies)
@@ -134,7 +172,7 @@ window.addEventListener('resize', () => {
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-    fireFliesMaterial.uniforms.uPixelRatio.value=Math.min(window.devicePixelRatio, 2)
+    fireFliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
 })
 
 /**
@@ -142,9 +180,9 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
+camera.position.x = 2
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
@@ -161,13 +199,13 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-debugObject.clearColor = '#ffffff'
+debugObject.clearColor = '#696363'
 renderer.setClearColor(debugObject.clearColor)
 gui
-.addColor(debugObject,'clearColor')
-.onChange(()=>{
-    renderer.setClearColor(debugObject.clearColor)
-})
+    .addColor(debugObject, 'clearColor')
+    .onChange(() => {
+        renderer.setClearColor(debugObject.clearColor)
+    })
 /**
  * Animate
  */
@@ -176,6 +214,9 @@ const clock = new THREE.Clock()
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
+    //Update material
+    fireFliesMaterial.uniforms.uTime.value = elapsedTime
+    portalLightMaterial.uniforms.uTime.value = elapsedTime
     // Update controls
     controls.update()
 
--- src/shaders/fireflies/vertex.glsl
+++ src/shaders/fireflies/vertex.glsl
@@ -1,12 +1,30 @@
 uniform float uPixelRatio;
+uniform float uSize;
+uniform float uTime;
+attribute float aScale;
 
 void main(){
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.1);
+
+ // Calculate rotation angle
+    float angle = 0.01; // 'speed' controls the rotation speed
+
+    // modelPosition.x += sin(uTime + modelPosition.x * 100.0) * aScale * 0.2;
+
+     // Rotate around a point
+    float rotatedX = cos(angle) * modelPosition.x - sin(angle) * modelPosition.z;
+    float rotatedZ = sin(angle) * modelPosition.x + cos(angle) * modelPosition.z;
+
+   float elevation = 0.2;
+   
+    modelPosition.x = rotatedX + sin(uTime + modelPosition.x * 100.0) * aScale * 0.2;
+    modelPosition.y += elevation + cos(uTime + modelPosition.y * 100.0) * aScale * 0.2;
+    modelPosition.z = rotatedZ;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
-
-    gl_PointSize = 40.0 * uPixelRatio;
+    gl_PointSize = uSize * aScale * uPixelRatio;
     gl_PointSize *= (1.0 / - viewPosition.z);
 }
